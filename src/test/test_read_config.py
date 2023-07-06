97f9587b68b41f679c14f8644e6ceaa6cf360b3d
Hunsinger, Heather Elizabeth <hhunsinger@usgs.gov>
2023-07-06 14:20:17 -0600

    Add read_config.py test coverage=72%

---
+++ src/test/test_read_config.py
@@ -0,0 +1,25 @@
+import configparser
+import pathlib
+
+from wasp.read_config import read_config
+
+
+def test_read_config():
+    # should fail if the config is not found or empty
+    try:
+        config = read_config()
+        raise Exception("This should have failed!")
+    except ValueError as e:
+        assert str(e).endswith(
+            "'PATHS' must be a section of the config file and include the options:\n"
+            "['code_path', 'surf_gf_bank', 'modelling', 'get_near_gf', 'compute_near_gf', 'info', 'cartopy_files']"
+        )
+
+    # should path if the config file is as expected
+    config = read_config(
+        config_path=pathlib.Path(__file__).parent.parent.parent
+        / "docs"
+        / "examples"
+        / "config.ini"
+    )
+    assert isinstance(config, configparser.ConfigParser)
--- src/wasp/read_config.py
+++ src/wasp/read_config.py
@@ -1,28 +1,70 @@
 # stdlib imports
 import configparser
 import pathlib
-import os
+from typing import Union
 
-folder = os.path.dirname(__file__)
-CONFIG_PATH = os.path.join(os.path.dirname(folder), 'config.ini')
-print(CONFIG_PATH)
+PROJECT_DIRECTORY = pathlib.Path(__file__).parent.parent.parent
+CONFIG_PATH = PROJECT_DIRECTORY / "config.ini"
 
 
-def read_config():
+def _validate_config(config: configparser.ConfigParser, config_path: pathlib.Path):
+    """Validate the config file
+
+    :param config: The config object
+    :type config: configparser.ConfigParser
+    :param config_path: The path to the config file
+    :type config_path: pathlib.Path
+    :raises ValueError: If a required section and/or option is missing
+    """
+    errors = []
+    options = [
+        "code_path",
+        "surf_gf_bank",
+        "modelling",
+        "get_near_gf",
+        "compute_near_gf",
+        "info",
+        "cartopy_files",
+    ]
+    if "PATHS" not in config.sections():
+        errors += [
+            (
+                "'PATHS' must be a section of the config file "
+                f"and include the options:\n{options}"
+            )
+        ]
+    else:
+        for option in options:
+            if option not in config.options("PATHS"):
+                errors += [f"Option in PATHS section '{option}' must be included"]
+    if len(errors):
+        error_str = "\n".join(errors)
+        raise ValueError(f"Error parsing config at {config_path}:\n{error_str}")
+
+
+def read_config(config_path: pathlib.Path = CONFIG_PATH) -> configparser.ConfigParser:
+    """Parse and validate the config file
+
+    :param config_path: The path to the config file, defaults to CONFIG_PATH
+    :type config_path: pathlib.Path, optional
+    :return: The config parser object
+    :rtype: configparser.ConfigParser
+    """
     config = configparser.ConfigParser()
-    config.read(CONFIG_PATH)
+    config.read(config_path)
+    _validate_config(config=config, config_path=config_path)
     return config
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = read_config()
     print(config.sections())
     # this will include an empty DEFAULT section,
     # which usefulness is described here:
     # https://www.enfoldsystems.com/software/proxy/docs/4.0/configuringmanually.html#the-default-section
     for section, options in config.items():
-        print('Options for section {section}:')
+        print("Options for section {section}:")
         for key, value in options.items():
-            print(key, value)#'\t{key} = {value}')
+            print(key, value)  #'\t{key} = {value}')
     # grabbing values
-    print(config['PATHS']['code_path'])
+    print(config["PATHS"]["code_path"])
