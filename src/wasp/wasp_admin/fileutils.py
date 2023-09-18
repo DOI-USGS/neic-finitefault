9bb209d4326afabd9edc1420dbf7c479e093fc8a
Hunsinger, Heather Elizabeth <hhunsinger@usgs.gov>
2023-09-18 13:13:58 -0600

    Review update

--- src/wasp/wasp_admin/fileutils.py
+++ src/wasp/wasp_admin/fileutils.py
@@ -4,9 +4,7 @@ from typing import List
 
 def validate_files(files: List[pathlib.Path]):
     "Validate files exist"
-    print("HERE", files)
     for file in files:
-        print("The file", file)
         if not file.exists():
             raise FileNotFoundError(
                 f"Expected file ({str(file.resolve())}) does not exist!"
