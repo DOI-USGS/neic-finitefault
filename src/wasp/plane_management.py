85e350f520e4f131ea3e4442e2046e957df6fac7
Hunsinger, Heather Elizabeth <hhunsinger@usgs.gov>
2023-09-21 13:08:33 -0600

    Remove unused script

--- src/wasp/plane_management.py
+++ src/wasp/plane_management.py
@@ -27,17 +27,3 @@ def __unpack_plane_data(
     hyp_stk = plane_info["hyp_stk"] - 1
     hyp_dip = plane_info["hyp_dip"] - 1
     return stk_subfaults, dip_subfaults, delta_strike, delta_dip, hyp_stk, hyp_dip
-
-
-if __name__ == "__main__":
-    import os
-
-    from wasp.get_outputs import read_solution_fsp_format
-
-    #    segments, rise_time, point_sources = __read_planes_info()
-    #    print(segments)
-    os.chdir(
-        "/home/pk/Inversion_Chen_Ji/Inversiones/20140401234647/paper_challa/pk.5/NP2/srcmod_solutions/"
-    )
-    tensor_info, solution, subfault = read_solution_fsp_format("hayes")
-    print(solution)
