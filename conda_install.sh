c70d1157fcdb539d461cfa57c273457e426809b5
Hunsinger, Heather Elizabeth <hhunsinger@usgs.gov>
2024-08-19 17:01:11 -0600

    Fix shellcheck

---
+++ conda_install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# ==============================================================================
+# Define usage
+#     Install conda (if needed)
+# ==============================================================================
+## define usage
+function usage {
+    echo "----------------------------------------------------------------"
+    echo "Install Conda"
+    echo "----------------------------------------------------------------"
+}
+
+## parse arguments
+REQUIRED_ARGS=()
+while [[ $# -gt 0 ]]; do
+    # shellcheck disable=SC2221,SC2222
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -*|--*)
+            echo "Invalid option $1"
+            usage
+            exit 1
+            ;;
+        *)
+            REQUIRED_ARGS+=("$1")
+            shift
+            ;;
+    esac
+done
+
+
+# shellcheck source=./install.d/conda.sh
+source ./install.d/conda.sh;
